5fa69cea039c5a60459d0ae68d1b2698a7830996
John <yohanskefyalew594@gmail.com>
2026-08-14 10:33:57 +0300

    fixed

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const app = require('./app');
+
 const PORT = process.env.PORT || 3000;
-if (process.env.NODE_ENV === 'production') {
-  console.error("CRITICAL ERROR: Failed to connect to core configuration service!");
-  process.exit(1);
-}
+
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
 });
